265444af34d54652be70cd69c08f3bb42c311f79
Roman Novikov <mystdeim@gmail.com>
2022-03-22 14:00:00 +0300

    fix condition

--- listener/src/main/kotlin/com/rarible/protocol/tezos/listener/handler/external/ExternalActivityEventHandler.kt
+++ listener/src/main/kotlin/com/rarible/protocol/tezos/listener/handler/external/ExternalActivityEventHandler.kt
@@ -9,9 +9,11 @@ import com.rarible.protocol.tezos.core.model.TezosOrderEventDto
 import com.rarible.protocol.tezos.dto.TezosActivitySafeDto
 import com.rarible.protocol.tezos.dto.TezosOrderSafeEventDto
 import com.rarible.protocol.tezos.listener.config.KafkaEventFactory.kafkaMsg
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
 @Component
+@ConditionalOnProperty(name = ["listener.enable"], havingValue = "true", matchIfMissing = false)
 class ExternalActivityEventHandler(
     val producer: RaribleKafkaProducer<TezosActivitySafeDto>,
     val commonProps: CommonProperties
--- listener/src/main/kotlin/com/rarible/protocol/tezos/listener/handler/external/ExternalOrderEventHandler.kt
+++ listener/src/main/kotlin/com/rarible/protocol/tezos/listener/handler/external/ExternalOrderEventHandler.kt
@@ -6,9 +6,11 @@ import com.rarible.protocol.tezos.core.config.CommonProperties
 import com.rarible.protocol.tezos.core.model.TezosOrderEventDto
 import com.rarible.protocol.tezos.dto.TezosOrderSafeEventDto
 import com.rarible.protocol.tezos.listener.config.KafkaEventFactory.kafkaMsg
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
 @Component
+@ConditionalOnProperty(name = ["listener.enable"], havingValue = "true", matchIfMissing = false)
 class ExternalOrderEventHandler(
     val producer: RaribleKafkaProducer<TezosOrderSafeEventDto>,
     val commonProps: CommonProperties

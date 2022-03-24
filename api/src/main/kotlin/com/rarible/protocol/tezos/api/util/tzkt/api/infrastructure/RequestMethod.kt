cfa355eca29224dbd6645987951a0d0d268e56af
Mika <michael.laporte@functori.com>
2022-03-24 11:02:40 +0100

    api: item_by_id with tzkt api

---
+++ api/src/main/kotlin/com/rarible/protocol/tezos/api/util/tzkt/api/infrastructure/RequestMethod.kt
@@ -0,0 +1,8 @@
+package com.rarible.protocol.tezos.api.util.tzkt.api.infrastructure
+
+/**
+ * Provides enumerated HTTP verbs
+ */
+enum class RequestMethod {
+    GET, DELETE, HEAD, OPTIONS, PATCH, POST, PUT
+}
